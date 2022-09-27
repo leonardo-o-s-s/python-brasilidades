52f4c27cf2ac2a9add8e528a47cbc35ce27ad546
leonardo-o-s-s <leo.oliveira.silva227@gmail.com>
2022-09-27 20:25:21 -0300

    criando modulo cpf.py

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+from cpf import Cpf
+
 cpf = "15616987913"
-tamanho_cpf = len(cpf)
-print(tamanho_cpf)
+
+objeto_cpf = Cpf(cpf)
+
+print(objeto_cpf)
+
+"""
+fatia_um = cpf[:3]
+fatia_dois = cpf[3:6]
+fatia_tres = cpf[6:9]
+fatia_quatro = cpf [9:]
+cpf_formatado = f"{fatia_um}.{fatia_dois}.{fatia_tres}-{fatia_quatro}"
+print(cpf_formatado)
+"""
