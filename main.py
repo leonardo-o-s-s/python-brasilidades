180c2cdf9da65d23fd1eb8203269a567b9095ad4
leonardo-o-s-s <leo.oliveira.silva227@gmail.com>
2022-09-27 21:06:41 -0300

    importando modulo validate para validar cpf

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-from cpf import Cpf
+from validate_docbr import CPF
+# rom cpf import Cpf
 
-cpf = "15616987913"
+cpf = CPF()
 
-objeto_cpf = Cpf(cpf)
-
-print(objeto_cpf)
-
-"""
-fatia_um = cpf[:3]
-fatia_dois = cpf[3:6]
-fatia_tres = cpf[6:9]
-fatia_quatro = cpf [9:]
-cpf_formatado = f"{fatia_um}.{fatia_dois}.{fatia_tres}-{fatia_quatro}"
-print(cpf_formatado)
-"""
+print(cpf.validate("12345678911"))
