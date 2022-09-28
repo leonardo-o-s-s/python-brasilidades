7783d14fe703de42b03aec7dd7a198b9b0b3d26b
leonardo-o-s-s <leo.oliveira.silva227@gmail.com>
2022-09-28 13:38:21 -0300

    alterando codigos para validar cpf e cnpj

--- cpf_cnpj.py
+++ cpf_cnpj.py
@@ -21,19 +21,25 @@ class CpfCnpj:
             raise ValueError("Documento inválido")
 
     def __str__(self):
-        return self.format_cpf()
+        if self.tipo_documento == "cpf":
+            return self.format_cpf()
+        elif self.tipo_documento == "cnpj":
+            return self.format_cnpj()
 
     def cpf_eh_valido(self, cpf):
         if len(cpf) == 11:
             validador = CPF()
             return validador.validate(cpf)
-
         else:
             raise ValueError("Quatidade inválida de dígitos")
 
     def format_cpf(self):
         mascara = CPF()
         return mascara.mask(self.cpf)
+
+    def format_cnpj(self):
+        mascara = CNPJ
+        return mascara.mask(self.cnpj)
 
     def cnpj_eh_valido(self, cnpj):
         if len(cnpj) == 14:
--- main.py
+++ main.py
@@ -5,7 +5,11 @@ from validate_docbr import CNPJ
 # cpf_um = Cpf("15316264754")
 # print(cpf_um)
 
-exemplo_cnpj = "35379838000112"
+exemplo_cnpj = "10360842000108"
+exemplo_cpf = "47913825871"
 # cnpj = CNPJ()
 # print(cnpj.validate((exemplo_cnpj)))
 documento = CpfCnpj(exemplo_cnpj, "cnpj")
+print(documento)
+documento2 = CpfCnpj(exemplo_cpf, "cpf")
+print(documento2)
