96d8b299e06692eb334fdf027692e6a57a539649
leonardo-o-s-s <leo.oliveira.silva227@gmail.com>
2022-09-28 13:09:45 -0300

    modificando nome para cpf_cnpj.py e colocando validacao na classe

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-from cpf import Cpf
+from cpf_cnpj import CpfCnpj
+from validate_docbr import CNPJ
 
-cpf_um = Cpf("15316264754")
-print(cpf_um)
 
+# cpf_um = Cpf("15316264754")
+# print(cpf_um)
+
+exemplo_cnpj = "35379838000112"
+# cnpj = CNPJ()
+# print(cnpj.validate((exemplo_cnpj)))
+documento = CpfCnpj(exemplo_cnpj, "cnpj")
