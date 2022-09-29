173a79c4374fa39907a44cf4890848728803c8ae
leonardo-o-s-s <leo.oliveira.silva227@gmail.com>
2022-09-29 19:22:40 -0300

    acessando api

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import requests
 from acesso_cep import BuscaEndereco
 
-cep = 25870145
+# r = requests.get("https://viacep.com.br/ws/01001000/json/")
+# print(r.text)
+
+cep = "02461000"
 objeto_cep = BuscaEndereco(cep)
-print(objeto_cep)
+#print(objeto_cep)
+objeto_cep.acessa_via_cep()
