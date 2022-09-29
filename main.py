27e6bbb2e06fd19ebfdb51f52ae628e12152a137
leonardo-o-s-s <leo.oliveira.silva227@gmail.com>
2022-09-29 15:02:44 -0300

    criando mascara para o numero de telefone

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ import re
 telefone = "552126481234"
 
 telefone_objeto = TelefonesBr(telefone)
+#padrao = "([0-9]{2,3})?([0-9]{2})?([0-9]{4,5})([0-9]{4})"
+#resposta = re.search(padrao, telefone)
+#print(resposta.group())
+
+print(telefone_objeto)
